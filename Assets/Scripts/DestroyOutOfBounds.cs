efbe5314667110147e050d34293c391c993a429a
emesefe <maria@frogames.es>
2023-10-31 12:51:46 +0100

    DestroyOutOfBounds

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    [SerializeField] private float topBound = 30f;
+    [SerializeField] private float bottomBound = -10f;
+
+    private void Update()
+    {
+        // Comida
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        // Animales
+        if (transform.position.z < bottomBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
